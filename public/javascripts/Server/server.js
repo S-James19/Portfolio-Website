aee0f2b9fe49fc00c53d6cecea765b255d3294e9
Sam James <samueljames2002@outlook.com>
2022-12-31 16:18:05 +0000

    added code to server.js to listen for requests

---
+++ public/javascripts/Server/server.js
@@ -0,0 +1,17 @@
+// code from - https://www.youtube.com/watch?v=LVfH5FDOa3o
+
+// access modules
+const express = require('express');
+const path = require('path');
+
+// access .env file storing username and password for mysql
+// https://stackoverflow.com/questions/69259896/set-environment-variables-outside-of-pages-dir-in-nextjs
+require('dotenv').config({path: path.resolve(__dirname, "../../../private/.env")});
+
+// create application
+const application = express();
+application.use(express.json());
+
+application.listen(process.env.ACCESS_PORT, () => {
+    console.log("server is running on port" + process.env.ACCESS_PORT);
+})
