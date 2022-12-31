7c187c16b41686d147a6704ade6a74335fe17854
Sam James <samueljames2002@outlook.com>
2022-12-31 15:55:07 +0000

    created folder setup for backend code

--- public/javascripts/Server/database/database.js
+++ public/javascripts/Server/database/database.js
@@ -1,11 +1,13 @@
-// https://www.w3schools.com/nodejs/nodejs_mysql.asp
 
 // import necessary packages for database connection
 const mysql = require('mysql2');
 const path = require('path');
 
 // access .env file storing username and password for mysql
+// https://stackoverflow.com/questions/69259896/set-environment-variables-outside-of-pages-dir-in-nextjs
 require('dotenv').config({path: path.resolve(__dirname, "../../private/.env")});
+
+// https://www.w3schools.com/nodejs/nodejs_mysql.asp
 
 // create connection with mysql
 const connection = mysql.createConnection(
@@ -16,6 +18,8 @@ const connection = mysql.createConnection(
         database: "cards"
     }
 );
+
+// https://www.w3schools.com/nodejs/nodejs_mysql_select.asp
 
 // connect to mysql server
 connection.connect(function(err) {
