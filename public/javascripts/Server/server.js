0e7b742fb58c2bc6404263256542fd3c84cbbba3
Sam James <samueljames2002@outlook.com>
2022-12-31 16:52:18 +0000

    added routes module to server.js

--- public/javascripts/Server/server.js
+++ public/javascripts/Server/server.js
@@ -3,6 +3,7 @@
 // access modules
 const express = require('express');
 const path = require('path');
+const apiRouter = require('./routes/router.js');
 
 // access .env file storing username and password for mysql
 // https://stackoverflow.com/questions/69259896/set-environment-variables-outside-of-pages-dir-in-nextjs
@@ -10,7 +11,9 @@ require('dotenv').config({path: path.resolve(__dirname, "../../../private/.env")
 
 // create application
 const application = express();
+
 application.use(express.json());
+application.use('/api/chirps' ,apiRouter);
 
 application.listen(process.env.ACCESS_PORT, () => {
     console.log("server is running on port" + process.env.ACCESS_PORT);
